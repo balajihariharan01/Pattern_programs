09b6957fed535189e4c0c7e44019890b262dbe82
BALAJI HARIHARAN N S <balajihariharanns@gmail.com>
2025-07-31 21:33:18 +0530

    pattern3 program

--- pattern3.java
+++ pattern3.java
@@ -5,7 +5,7 @@ public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int rows = sc.nextInt();
-
+  
     for (int i = 1; i <= rows; ++i) {
       for (int j = 1; j <= i; ++j) {
         System.out.print("* ");
